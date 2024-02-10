695e93e3006aaa58b7584dd2d73553aa512d275f
Nicholas Collin Merritt <107739492+Subpoened-peon@users.noreply.github.com>
2024-02-10 14:31:50 -0700

    Closes task #53
    Added linting tests for initial use and prepared it for future testing
    
    [t:15]

--- test.sh
+++ test.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-exit 127
+
+VERSION="0.1"
+
+# Additional tests to be added here...
+
+# lint tests here are assuming that these are the primary files being used for the extension (.json, .css, .html, .css)
+
+# js lint
+docker run HookLineSecureImageV:$Version sh -c "eslint ./*.js"
+
+# css lint
+docker run HookLineSecureImageV:$Version sh -c "stylelint ./*.css"
+
+# HTML lint
+docker run HookLineSecureImageV:$Version sh -c "htmlhint ./*.html"
+
+# JSON lint
+docker run HookLineSecureImageV:$Version sh -c "jsonlint ./*.json"
+
+echo "tests complete"
