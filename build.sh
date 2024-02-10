85b8d337458200635b78665859bc5c4fc6fa601a
Nicholas Collin Merritt <107739492+Subpoened-peon@users.noreply.github.com>
2024-02-09 21:01:28 -0700

    Closes task #52
    To be changed in the future for typical developer extension packaging, but unsigned zip used for now. Along with docker image for testing
    
    
    [t:55]

--- build.sh
+++ build.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-exit 127
+
+VERSION="0.1"
+
+# I have read online about chrome extensions being packaged with a .crx file and a key .pem fle, but for now so we can
+# have a working build.sh file I will just have be packaged in a .zip file without signing it.
+echo "Creating zip file..."
+zip -r HookLineSecureV:$VERSION.zip ./* --exclude .github/*
+
+# Build Docker image
+echo "Building Docker image..."
+docker build -t HookLineSecureImageV:$VERSION .
+
+echo "Build completed successfully!"
