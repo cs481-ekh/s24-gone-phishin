b98e9f11d73582bb03e5be31f2a62f7e0c77e9cc
Subpoened-peon <www.nic.merritt@gmail.com>
2024-02-14 18:53:32 -0700

    yaml files updated

--- Containerfile
+++ Containerfile
@@ -6,12 +6,14 @@ RUN npm install -g eslint stylelint htmlhint jsonlint
 
 # Install OpenSSL
 RUN apt-get update && \
-    apt-get install -y openssl
+    apt-get install -y openssl zip
 
 # Set the working directory in the container
 WORKDIR /app
 
 COPY . .
 
+run chmod +x build.sh
+run chmod +x test.sh
 # Define the command to run when the container starts
 CMD ["bash"]
--- build.sh
+++ build.sh
@@ -2,10 +2,15 @@
 
 VERSION="0.1"
 
-# I have read online about chrome extensions being packaged with a .crx file and a key .pem fle, but for now so we can
-# have a working build.sh file I will just have be packaged in a .zip file without signing it.
-echo "Creating zip file..."
-zip -r HookLineSecureV:$VERSION.zip ./* --exclude .github/*
+SOURCE_DIR="${GITHUB_WORKSPACE}"
+OUTPUT_DIR="${GITHUB_WORKSPACE}/build"
+
+# Ensure the output directory exists
+mkdir -p "${OUTPUT_DIR}"
+
+# Change directory to the source directory
+cd "${SOURCE_DIR}" || exit
+zip -r "${OUTPUT_DIR}/HookLineSecureV:$VERSION.zip ./* --exclude .github/*
 
 # additional signing required to be added later...
 
