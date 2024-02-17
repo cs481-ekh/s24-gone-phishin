ab49b3015a4042677695356764a3af3ac234ba97
torisimon2 <108162081+torisimon2@users.noreply.github.com>
2024-02-17 15:43:52 -0700

    Html and plaintext compatibility (#90)
    * Updates #63, updates #65
    
    * tester runs, updates, #63 updates #65
    
    * quick update
    
    * process tests all run
    
    * updates #63, updates #65
    
    * closes #63, closes #65

---
+++ tests/unit-tests/process-content.test.js
@@ -0,0 +1,41 @@
+/**
+ * Test script for process-content.js
+ * 
+ * @author Tori Simon
+ */
+const process = require("../../js/process-content");
+
+//Test if can recognize HTML
+test('Test if HTML is recognized', () => {
+    expect(process.isHTML("<div>Hello <b>world</b>!</div>")).toBe(true); 
+});
+
+//Test if can recongnize plaintext
+test('Test if plaintext is recognized', () => {
+    expect(process.isHTML("Hello world!")).toBe(false);
+});
+
+//Check for false html positives
+test('For false positive HTML', () => {
+    expect(process.isHTML("three is < four")).toBe(false);
+});
+
+//Check if extract extracts correctly
+test('Test if HTML is extracted correctly', () => {
+    expect(process.extractTextFromHTML("<div>Hello <b>world</b>!</div>")).toEqual("Hello world!");
+});
+
+//Check if normalize normalizes correctly
+test('Test if trailing whitespace is removed', () => {
+    expect(process.normalizeText("        Hello world!      ")).toEqual("Hello world!");
+});
+
+//test process content with HTML
+test('Test if process content works with HTML', () => {
+    expect(process.processContent("<div>Hello <b>world</b>!</div>")).toEqual("Hello world!");
+});
+
+//test process content with plaintext
+test('Test if process content works with plaintext', () => {
+    expect(process.processContent("Hello world!")).toEqual("Hello world!");
+});
