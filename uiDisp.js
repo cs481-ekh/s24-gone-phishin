f1acd4c3001bb5ae93de2c1b2a3e9c08d97d0583
torisimon2 <torisimon@u.boisestate.edu>
2024-02-29 09:28:32 -0700

    updates #60

--- uiDisp.js
+++ uiDisp.js
@@ -97,4 +97,41 @@ tab.addEventListener('click', () => {
     subtree: true,
     childList: true,
   });
+
+    //Call LangaugeTool API to check for spelling errors
+    const params = new URLSearchParams();
+    params.append("text", document.querySelector('.a3s.aiL').textContent);
+    console.log(params.toString()); //FIXME debug
+    fetch("https://api.languagetoolplus.com/v2/check", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Accept": "application/json"
+            },
+            body: params.toString() + "&language=en-US&enabledOnly=false"
+        })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log("API Response:", data);
+          const matchesArray = data.matches; // Extracting the matches array
+          console.log("Matches:", matchesArray);
+          let spellingErrors = [];
+          let grammarErrors = [];
+          matchesArray.forEach(error => {
+            if(error.type = "misspelling") {
+              spellingErrors.push(error)
+            }
+            else {
+              grammarErrors.push(error)
+            }
+          })
+          const spellingCount = spellingErrors ? spellingErrors.length : 0;
+          const grammarCount = grammarErrors ? grammarErrors.length : 0;
+          smBodyDiv.textContent = JSON.stringify(spellingCount, null, 2);
+        })
 });
