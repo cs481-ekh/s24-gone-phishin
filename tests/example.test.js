27ac9f1ade8c5d8d7edb8b7690e48f60363b7b22
SorrenMosher <sorrenmosher@gmail.com>
2024-03-04 14:56:56 -0700

    Closes #104

--- tests/example.test.js
+++
@@ -1,9 +0,0 @@
-// This test is an example on how to format future tests
-// imported fuctions go at the top, for example if a sum function existed:
-// const sum = require('./sum')
-
-// The test itself 
-test('description of test', () => {
-    expect(1 + 2).toBe(3); // typicaly the expect contains some function call:
-    // expect(sum(1, 2)).toBe(3)
-});
